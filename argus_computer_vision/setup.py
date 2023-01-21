b50542bb813f3d0ebb4011bcfc2701b299af154a
MedChebbi <1.mohamed.chebbi@gmail.com>
2023-01-21 13:28:33 +0100

    initial commit

---
+++ argus_computer_vision/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'argus_computer_vision'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mohamed',
+    maintainer_email='1.mohamed.chebbi@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
